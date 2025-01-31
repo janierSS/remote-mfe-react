b6f9f655933c8097a5d5e562508d212cacd27bc2
Janier <janierss17@gmail.com>
2025-01-30 22:22:02 -0500

    Add MF and exposes component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "remoteMfeReact",
+      filename: "remoteEntry.js",
+      // remotes: {
+      //   notesHomeRemote: 'http://localhost:5000/dist/assets/remoteEntry.js',
+      //   loginRemote: 'http://localhost:5001/dist/assets/remoteEntry.js'
+      // },
+      exposes: {
+        "./Card": "./src/components/Card",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  server: {
+    open: true,
+    port: 5000,
+  },
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
